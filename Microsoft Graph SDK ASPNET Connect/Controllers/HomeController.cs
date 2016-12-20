f7fb0b1b8d953645d918ed08f2f0f8586282721e
Leonel Nóbrega <lnobrega@uma.pt>
2016-12-20 01:24:51 +0000

    Add project files.

---
+++ Microsoft Graph SDK ASPNET Connect/Controllers/HomeController.cs
@@ -0,0 +1,146 @@
+﻿/* 
+*  Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. 
+*  See LICENSE in the source repository root for complete license information. 
+*/
+
+using System;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Microsoft_Graph_SDK_ASPNET_Connect.Helpers;
+using Microsoft_Graph_SDK_ASPNET_Connect.Models;
+using Resources;
+
+#if GRAPH
+using Microsoft.Graph;
+#endif
+
+namespace Microsoft_Graph_SDK_ASPNET_Connect.Controllers
+{
+    public class HomeController : Controller
+    {
+        public GraphService GraphService { get; } = new GraphService();
+
+        public ActionResult Index()
+        {
+            return View("Graph");
+        }
+
+        // Controller actions
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+#if GRAPH
+
+        [Authorize]
+        // Get the current user's email address from their profile.
+        public async Task<ActionResult> GetMyEmailAddress()
+        {
+            try
+            {
+
+                // Initialize the GraphServiceClient.
+                GraphServiceClient graphClient = SDKHelper.GetAuthenticatedClient();
+
+                // Get the current user's email address. 
+                ViewBag.Email = await mGraphService.GetMyEmailAddress(graphClient);
+                return View("Graph");
+            }
+            catch (ServiceException se)
+            {
+                if (se.Error.Message == Resource.Error_AuthChallengeNeeded) return new EmptyResult();
+                return RedirectToAction("Index", "Error", new { message = Resource.Error_Message + Request.RawUrl + ": " + se.Error.Message });
+            }
+        }
+
+        [Authorize]
+        // Send mail on behalf of the current user.
+        public async Task<ActionResult> SendEmail()
+        {
+            if (string.IsNullOrEmpty(Request.Form["email-address"]))
+            {
+                ViewBag.Message = Resource.Graph_SendMail_Message_GetEmailFirst;
+                return View("Graph");
+            }
+
+            // Build the email message.
+            Message message = mGraphService.BuildEmailMessage(Request.Form["recipients"], Request.Form["subject"]);
+            try
+            {
+
+                // Initialize the GraphServiceClient.
+                GraphServiceClient graphClient = SDKHelper.GetAuthenticatedClient();
+
+                // Send the email.
+                await mGraphService.SendEmail(graphClient, message);
+
+                // Reset the current user's email address and the status to display when the page reloads.
+                ViewBag.Email = Request.Form["email-address"];
+                ViewBag.Message = Resource.Graph_SendMail_Success_Result;
+                return View("Graph");
+            }
+            catch (ServiceException se)
+            {
+                if (se.Error.Message == Resource.Error_AuthChallengeNeeded) return new EmptyResult();
+                return RedirectToAction("Index", "Error", new { message = Resource.Error_Message + Request.RawUrl + ": " + se.Error.Message });
+           }
+        }
+#elif REST
+
+        [Authorize]
+        // Get the current user's email address from their profile.
+        public async Task<ActionResult> GetMyEmailAddress()
+        {
+            try
+            {
+                // Get an access token.
+                string accessToken = await SampleAuthProvider.Instance.GetUserAccessTokenAsync();
+
+                // Get the current user's email address. 
+                ViewBag.Email = await GraphService.GetMyEmailAddress(accessToken);
+                return View("Graph");
+            }
+            catch (Exception e)
+            {
+                if (e.Message == Resource.Error_AuthChallengeNeeded) return new EmptyResult();
+                return RedirectToAction("Index", "Error", new { message = Resource.Error_Message + Request.RawUrl + ": " + e.Message });
+            }
+        }
+
+        [Authorize]
+        // Send mail on behalf of the current user.
+        public async Task<ActionResult> SendEmail()
+        {
+            if (string.IsNullOrEmpty(Request.Form["email-address"]))
+            {
+                ViewBag.Message = Resource.Graph_SendMail_Message_GetEmailFirst;
+                return View("Graph");
+            }
+
+            // Build the email message.
+            MessageRequest email = GraphService.BuildEmailMessage(Request.Form["recipients"], Request.Form["subject"]);
+            try
+            {
+
+                // Get an access token.
+                string accessToken = await SampleAuthProvider.Instance.GetUserAccessTokenAsync();
+
+                // Send the email.
+                ViewBag.Message = await GraphService.SendEmail(accessToken, email);
+
+                // Reset the current user's email address and the status to display when the page reloads.
+                ViewBag.Email = Request.Form["email-address"];
+                return View("Graph");
+            }
+            catch (Exception e)
+            {
+                if (e.Message == Resource.Error_AuthChallengeNeeded) return new EmptyResult();
+                return RedirectToAction("Index", "Error", new { message = Resource.Error_Message + Request.RawUrl + ": " + e.Message });
+            }
+        }
+
+#endif
+    }
+}
